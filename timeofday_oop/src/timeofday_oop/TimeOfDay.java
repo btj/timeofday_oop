9a4ba41515654ace19c3b1daaae6f811b7d94293
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-02-28 11:36:47 +0100

    Mutable TimeOfDay using minutesSinceMidnight

--- timeofday_oop/src/timeofday_oop/TimeOfDay.java
+++ timeofday_oop/src/timeofday_oop/TimeOfDay.java
@@ -3,21 +3,16 @@ package timeofday_oop;
 public class TimeOfDay {
 	/**
 	 * @invar {@code hours) is between 0 and 23
-	 *     | 0 <= hours && hours <= 23 
+	 *     | 0 <= minutesSinceMidnight && minutesSinceMidnight < 24 * 60
 	 */
-	private int hours;
-	/**
-	 * @invar {@code minutes} is between 0 and 59
-	 *     | 0 <= minutes && minutes <= 59
-	 */
-	private int minutes;
+	private int minutesSinceMidnight;
 	
 	public int getHours() {
-		return this.hours;
+		return minutesSinceMidnight / 60;
 	}
 	
 	public int getMinutes() {
-		return this.minutes;
+		return minutesSinceMidnight % 60;
 	}
 	
 	/**
@@ -31,7 +26,7 @@ public class TimeOfDay {
 	public void setHours(int value) {
 		if (!(0 <= value && value <= 23))
 			throw new IllegalArgumentException("value is out of range");
-		this.hours = value;
+		this.minutesSinceMidnight = getMinutes() + value * 60;
 	}
 	
 	/**
@@ -46,6 +41,6 @@ public class TimeOfDay {
 	public void setMinutes(int value) {
 		if (!(0 <= value && value <= 59))
 			throw new IllegalArgumentException("value is out of range");
-		this.minutes = value;
+		this.minutesSinceMidnight = getHours() * 60 + value;
 	}
 }
